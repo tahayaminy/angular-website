4bbdc9c3849f2d0c33f703fbda183f843bfe2e03
Anonymous <ParsWebDesigners@gmail.com>
2022-01-19 18:34:39 +0330

    add in navbar->contents & description

--- src/app/course/tabbar/tabbar.component.ts
+++ src/app/course/tabbar/tabbar.component.ts
@@ -12,5 +12,14 @@ export class TabbarComponent implements OnInit {
   ngOnInit(): void {
   }
     active = 1;
-  
+    public isCollapsed1 = false;
+    public isCollapsed2 = true;
+    public isCollapsed3 = true;
+    isCollapsed=false;
+    handleToggle(el:boolean){
+      this.isCollapsed1 = false;
+      this.isCollapsed2 = false;
+      this.isCollapsed3 = false;
+      el=true;
+    }
 }
